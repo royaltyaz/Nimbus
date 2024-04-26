8a8cb18544e1476b0338101977b50af429ba3c60
aznpr <aznpr@Nimbus3000.lan>
2024-04-26 10:24:48 -0400

    First commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+
+
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        System.out.println("Hi There");
+        System.out.println("How are you today?");
+        System.out.println("I am fine, thanks");
+    
+    }
+}
